fa63f7ac3498e4e48a21251fc5001d9396eb65b5
Paula <paulagarizcun@gmail.com>
2018-10-07 11:51:09 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,15 @@ import { app_routing } from '../app/app.routes';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-// import { AboutComponent } from './components/about/about.component';
+import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-// import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    // AboutComponent,
+    AboutComponent,
     HeroesComponent
   ],
   imports: [
